a4d8f3da1cdb6e4456014095da6b8941018758fc
Valerii Viun <2455521@gmail.com>
2023-05-30 13:32:35 +0300

    added components

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,33 @@
+import { StatisticsList, StatisticsItem } from './Statistics.styled';
+import PropTypes from 'prop-types';
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  const statList = [
+    { statItem: good, title: 'Good' },
+    { statItem: neutral, title: 'Neutral' },
+    { statItem: bad, title: 'Bad' },
+    { statItem: total, title: 'Total feedbacks' },
+    { statItem: positivePercentage, title: 'Positive feedbacks' },
+  ];
+  return (
+    <StatisticsList>
+      {statList.map(({ statItem, title }) => (
+        <StatisticsItem key={title}>
+          <p>
+            {title} :<span>{statItem}</span>
+          </p>
+        </StatisticsItem>
+      ))}
+    </StatisticsList>
+  );
+};
+
+export default Statistics;
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
